93ab13b982f5da8ba621dfa60b2853693930219e
wk-nj <15380314863@163.com>
2020-04-30 16:46:03 +0800

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -4,3 +4,8 @@ function route_class()
 {
     return str_replace('.', '-', \Illuminate\Support\Facades\Route::currentRouteName());
 }
+
+function category_nav_active($category_id)
+{
+    return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
