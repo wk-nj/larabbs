94a88829164224bc15e6828831e8028de16b52e2
wk-nj <15380314863@163.com>
2020-05-07 16:57:54 +0800

    话题的编辑和删除

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,9 @@ class User extends Authenticatable implements MustVerifyEmailContracts
     {
         return $this->hasMany(Topic::class);
     }
+
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
 }
