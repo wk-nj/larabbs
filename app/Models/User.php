1da9769ed42ce7f77a0bafa1fc75e33a979861e6
wk-nj <15380314863@163.com>
2020-05-09 17:11:04 +0800

    集成 laravel-permission 扩展包

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmailContracts
 {
-    use MustVerifyEmailTrait, SoftDeletes;
+    use MustVerifyEmailTrait, SoftDeletes, HasRoles;
 
     use Notifiable {
         notify as protected laravelNotify;
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -48,5 +48,12 @@ class UsersTableSeeder extends Seeder
         $user->email = 'jeff@parse.cn';
         $user->avatar = 'http://larabbs.test/uploads/images/avatar/202004/28/1_1588067228_Hwmboy0Y3A.jpeg';
         $user->save();
+
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
+
+        // 将 2 号用户指派为『管理员』
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
     }
 }
